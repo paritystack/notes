fbf27e4f0dce97dd6dd3b2f2723a91cae396b751
paritystack <paritystack@users.noreply.github.com>
2026-06-01 11:45:09 +0000

    deploy: ae7323719f6f92359534bea8a5c67217d2e2f073

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f04597d6.js',
+                path_to_root + 'searchindex-63f00aed.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
